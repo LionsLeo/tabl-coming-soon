22b4c2f923cb9630e3f40ce1801b9c4fcafd5667
Meet Gada <gada.md@somaiya.edu>
2025-05-10 19:48:56 +0530

    deploy changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/', // optional, only if your project is not on a custom domain
+  
 };
 
 export default nextConfig;
