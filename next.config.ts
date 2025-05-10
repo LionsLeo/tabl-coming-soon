2532713cf9fa17d2e74a1401f0fe238fb718d97d
Meet Gada <gada.md@somaiya.edu>
2025-05-10 19:53:19 +0530

    added static files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '', // optional, only if your project is not on a custom domain
-};
-
-export default nextConfig;
